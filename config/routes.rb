e20c65ba919a0d8acfb37196b745bd864e5b492a
Mbele Lebohang <micheallm95@gmail.com>
2017-06-21 16:22:49 +0200

    pushing to remote

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   	get '/contact' ,to: 'static_pages#contact'
 
+  	get '/services' ,to: 'static_pages#services'
+
   	root 'static_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
